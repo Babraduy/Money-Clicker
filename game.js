d1b0a32692f9575854a846dd5cd3e8db79020c43
Babraduy <romansuccessful@gmail.com>
2024-03-14 19:48:42 +0100

    Updated game.js

--- game.js
+++ game.js
@@ -1,25 +1,19 @@
-if (JSON.parse(localStorage.getItem("money")) !== null) var money = JSON.parse(localStorage.getItem("money"));
+if (localStorage.getItem("money") !== null) var money = JSON.parse(localStorage.getItem("money"));
 else var money = 0;
 
 // mps - money per second
-if (JSON.parse(localStorage.getItem("mps")) !== null) var mps = JSON.parse(localStorage.getItem("mps"));
+if (localStorage.getItem("mps") !== null) var mps = JSON.parse(localStorage.getItem("mps"));
 else var mps = 0;
 
-if (JSON.parse(localStorage.getItem("mps_price")) !== null) var mps_price = JSON.parse(localStorage.getItem("mps_price"));
+if (localStorage.getItem("mps_price") !== null) var mps_price = JSON.parse(localStorage.getItem("mps_price"));
 else var mps_price = 10;
 
 // mpc - money per click
-if (JSON.parse(localStorage.getItem("mpc")) !== null) var mpc = JSON.parse(loalStorage.getItem("mpc"));
+if (localStorage.getItem("mpc") !== null) var mpc = JSON.parse(localStorage.getItem("mpc"));
 else var mpc = 1;
 
-if (JSON.parse(localStorage.getItem("mpc_price")) !== null) var mpc_price = JSON.parse(localStorage.getItem("mpc_price"));
+if (localStorage.getItem("mpc_price") !== null) var mpc_price = JSON.parse(localStorage.getItem("mpc_price"));
 else var mpc_price = 10;
-
-document.getElementById("money").innerHTML = money;
-document.getElementById("mps_price").innerHTML = mps_price;
-document.getElementById("mpc_price").innerHTML = mpc_price;
-document.getElementById("mps").innerHTML = mps;
-document.getElementById("mpc").innerHTML = mpc;
 
 function reset_progress() {
     money = 0;
@@ -27,26 +21,27 @@ function reset_progress() {
     mpc = 1;
     mps_price = 10;
     mpc_price = 10;
-    document.getElementById("money").innerHTML = money;
-    document.getElementById("mps_price").innerHTML = mps_price;
-    document.getElementById("mpc_price").innerHTML = mpc_price;
-    document.getElementById("mps").innerHTML = mps;
-    document.getElementById("mpc").innerHTML = mpc;
 }
 
 setInterval(() => {
     money+=mps;
     document.getElementById("money").innerHTML = money;
+    document.getElementById("mps_price").innerHTML = mps_price;
+    document.getElementById("mpc_price").innerHTML = mpc_price;
+    document.getElementById("mps").innerHTML = mps;
+    document.getElementById("mpc").innerHTML = mpc;
 
     // save
     localStorage.setItem('money', JSON.stringify(money));
+    localStorage.setItem('mpc', JSON.stringify(mpc));
+    localStorage.setItem('mps', JSON.stringify(mps));
+    localStorage.setItem('mpc_price', JSON.stringify(mpc_price));
+    localStorage.setItem('mps_price', JSON.stringify(mps_price));
 }, 1000)
 
 function add_money() {
     money+=mpc;
     document.getElementById("money").innerHTML = money;
-    
-    localStorage.setItem('money', JSON.stringify(money));
 }
 
 function show(name) {
@@ -60,29 +55,19 @@ function buy(name) {
     if (name == "mps" && money >= mps_price) {
         money -= mps_price;
         document.getElementById("money").innerHTML = money;
-        
-        localStorage.setItem('money', JSON.stringify(money));
 
         mps++;
         mps_price *= 2;
         document.getElementById("mps_price").innerHTML = mps_price;
         document.getElementById("mps").innerHTML = mps;
-        
-        localStorage.setItem('mps_price', JSON.stringify(mps_price));
-        localStorage.setItem('mps', JSON.stringify(mps));
     }
     if (name == "mpc" && money >= mpc_price) {
         money -= mpc_price;
         document.getElementById("money").innerHTML = money;
-        
-        localStorage.setItem('money', JSON.stringify(money));
 
         mpc++;
         mpc_price *= 3;
         document.getElementById("mpc_price").innerHTML = mpc_price;
         document.getElementById("mpc").innerHTML = mpc;
-        
-        localStorage.setItem('mpc_price', JSON.stringify(mpc_price));
-        localStorage.setItem('mpc', JSON.stringify(mpc));
     }
 }
